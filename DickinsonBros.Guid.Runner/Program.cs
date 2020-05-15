83f53798da4ce4d3b745bb598cf46ff06990b9fc
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-15 00:54:22 -0700

    Updated Test.yml, Added Runner

---
+++ DickinsonBros.Guid.Runner/Program.cs
@@ -0,0 +1,92 @@
+﻿using DickinsonBros.Guid.Abstractions;
+using DickinsonBros.Guid.Runner.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DickinsonBros.Guid.Runner
+{
+    class Program
+    {
+        IConfiguration _configuration;
+        async static Task Main(string[] args)
+        {
+            await new Program().DoMain();
+        }
+        async Task DoMain()
+        {
+            try
+            {
+                using (var applicationLifetime = new ApplicationLifetime())
+                {
+                    var services = InitializeDependencyInjection();
+                    ConfigureServices(services, applicationLifetime);
+
+                    using (var provider = services.BuildServiceProvider())
+                    {
+                        var guidService = provider.GetRequiredService<IGuidService>();
+
+                        Console.WriteLine
+                        (
+$@"Guid 1: 
+{guidService.NewGuid()}
+
+Guid 2: 
+{guidService.NewGuid()}
+
+Guid 3:
+{guidService.NewGuid()}
+
+"
+                        );
+                    }
+                    applicationLifetime.StopApplication();
+                    await Task.CompletedTask.ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("End...");
+                Console.ReadKey();
+            }
+        }
+
+        private void ConfigureServices(IServiceCollection services, ApplicationLifetime applicationLifetime)
+        {
+            services.AddOptions();
+            services.AddLogging(config =>
+            {
+                config.AddConfiguration(_configuration.GetSection("Logging"));
+
+                if (Environment.GetEnvironmentVariable("BUILD_CONFIGURATION") == "DEBUG")
+                {
+                    config.AddConsole();
+                }
+            });
+            services.AddSingleton<IApplicationLifetime>(applicationLifetime);
+            services.AddSingleton<IGuidService, GuidService>();
+        }
+
+        IServiceCollection InitializeDependencyInjection()
+        {
+            var aspnetCoreEnvironment = Environment.GetEnvironmentVariable("BUILD_CONFIGURATION");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{aspnetCoreEnvironment}.json", true);
+            _configuration = builder.Build();
+            var services = new ServiceCollection();
+            services.AddSingleton(_configuration);
+            return services;
+        }
+    }
+}
+
