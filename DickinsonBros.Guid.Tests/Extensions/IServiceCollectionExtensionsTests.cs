203d00d7134cbcbb310b87d56ddf69c0a526ae5b
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-25 14:42:57 -0700

    Develop (#5)
    * Updated Test.yml, Added Runner
    
    * Update
    
    * Test Merge
    
    * Updated Test.YML - Trigger build from any file on pull request to master
    
    * Update Packages
    
    * Update Version
    
    * Revert Version
    
    * Added Extension Method
    
    * Update README.md

---
+++ DickinsonBros.Guid.Tests/Extensions/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using DickinsonBros.Guid.Abstractions;
+using DickinsonBros.Guid.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DickinsonBros.Guid.Tests.Extensions
+{
+    [TestClass]
+    public class IServiceCollectionExtensionsTests
+    {
+        [TestMethod]
+        public void AddPropelAPI_Should_Succeed()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.AddGuidService();
+
+            // Assert
+            Assert.IsTrue(serviceCollection.Any(serviceDefinition => serviceDefinition.ServiceType == typeof(IGuidService) &&
+                                                       serviceDefinition.ImplementationType == typeof(GuidService) &&
+                                                       serviceDefinition.Lifetime == ServiceLifetime.Singleton));
+        }
+    }
+}
---
+++ DickinsonBros.Guid/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DickinsonBros.Guid.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DickinsonBros.Guid.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGuidService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.TryAddSingleton<IGuidService, GuidService>();
+            return serviceCollection;
+        }
+    }
+}
